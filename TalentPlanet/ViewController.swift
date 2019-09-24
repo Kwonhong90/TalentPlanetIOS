1c8b30afba59d5a7459e2c74c1775e4cb3e47e6d
Kwonhong Min <mkh9012@kookmin.ac.kr>
2019-09-24 10:34:41 +0900

    Initial Commit

---
+++ TalentPlanet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TalentPlanet
+//
+//  Created by 민권홍 on 24/09/2019.
+//  Copyright © 2019 민권홍. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
